6c4a60693e414a8dccd274f9dabe1f0164aa59e2
Salil Joshi <salilsj1619@gmail.com>
2023-06-13 13:44:08 +0530

    Commit 01 by user 01

--- FileFirst.java
+++ FileFirst.java
@@ -3,13 +3,8 @@ class FileFirst
 
     public static void main(String[] args) {
         
-        System.out.println("Hello");
-
-
-         System.out.println("This is merged line");
-
-
-          System.out.println("This is first Commit");
+      System.out.println("THis is first line by first user");
+          
 
 
     }
